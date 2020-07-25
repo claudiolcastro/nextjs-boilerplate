22772b0f11789c6729ee5f56cb5c27e44694c4a6
Claudio Castro <claudioluiz.castro@gmail.com>
2020-07-25 19:47:04 -0300

    config storybook

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,9 +1,9 @@
 import * as S from './styles';
 
-const Main = () => {
+const Main = ({ title = 'React Avançado' }) => {
   return (
     <S.Wrapper>
-      <h1>React Avançado</h1>
+      <h1>{title}</h1>
     </S.Wrapper>
   );
 };
---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,10 @@
+import { withKnobs, text } from '@storybook/addon-knobs';
+import Main from '.';
+
+export default {
+  title: 'Main',
+  component: Main,
+  decorators: [withKnobs]
+};
+
+export const Basic = () => <Main title={text('Title', 'React Avançado')} />;
